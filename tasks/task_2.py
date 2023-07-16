a89452ab50c430690dd7c8935c791d09a226b92e
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-07-16 14:21:14 +0100

    Домашнее задание | 2. Консольные операции
    - Разбор задач: 2, 9

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,4 +1,7 @@
-print("Таблица умножения:")
-print('1\t' '2\t' '3\t')
-print('2\t' '4\t' '6\t')
-print('3\t' '6\t' '9\t')
+print('Таблица умножения:')
+
+tab: str = '\t\t'
+
+print('1', '2', '3', sep=tab)
+print('2', '4', '6', sep=tab)
+print('3', '6', '9', sep=tab)
--- tasks/task_9.py
+++ tasks/task_9.py
@@ -4,7 +4,12 @@ kilo_size: float = 123456789 / 1024
 mega_size: float = kilo_size / 1024
 giga_size: float = mega_size / 1024
 
-print('Размер в байтах:', byte_size)
-print('Размер в килобайтах:', kilo_size)
-print('Размер в мегабайтах:', mega_size)
-print('Размер в гигабайтах:', giga_size)
+# print('Размер в байтах:', byte_size)
+# print('Размер в килобайтах:', kilo_size)
+# print('Размер в мегабайтах:', mega_size)
+# print('Размер в гигабайтах:', giga_size)
+
+print('Размер в байтах: ' + str(byte_size),
+      'Размер в килобайтах: ' + str(kilo_size),
+      'Размер в мегабайтах: ' + str(mega_size),
+      'Размер в гигабайтах: ' + str(giga_size), sep='\n')
