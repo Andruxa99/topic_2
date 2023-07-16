cb1a4b5a08468bdf98c35bebd596954744444c86
Andruxa99 <andrei1999311@gmail.com>
2023-07-16 15:38:39 +0300

    До делал:)

--- tasks/task_2.py
+++ tasks/task_2.py
@@ -1,4 +1,4 @@
-print(...)
-print(...)
-print(...)
-print(...)
+print("Таблица умножения:")
+print('1\t' '2\t' '3\t')
+print('2\t' '4\t' '6\t')
+print('3\t' '6\t' '9\t')
--- tasks/task_9.py
+++ tasks/task_9.py
@@ -1,11 +1,10 @@
 byte_size: int = 1234567890
 
-kilo_size = ...
-mega_size = ...
-giga_size = ...
+kilo_size: float = 123456789 / 1024
+mega_size: float = kilo_size / 1024
+giga_size: float = mega_size / 1024
 
-print(byte_size)
-print(kilo_size)
-print(mega_size)
-print(giga_size)
-
+print('Размер в байтах:', byte_size)
+print('Размер в килобайтах:', kilo_size)
+print('Размер в мегабайтах:', mega_size)
+print('Размер в гигабайтах:', giga_size)
